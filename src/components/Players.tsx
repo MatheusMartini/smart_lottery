114b5a20e9058a04d2196fbb413cca5d7adc9d04
Matheus Martini <matheus.martini@rede.ulbra.br>
2022-05-19 17:47:01 -0300

    update layout

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -43,32 +43,32 @@ return !contract ? null : (
       
           {/* card players */}
           <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6 ">
-            
             <div className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-20 ">
 
                 {/* title */}
-              <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7 ">
-                Players Lottery 1
+              <p className=" text-black-600 font-medium capitalize my-2 sm:my-7 ">
+              {players.length} Address in Lottery 1
+                
               </p>
 
                 {/* list players */}
 
-              <div className="h-60 overflow-y-auto ... lg:py-1 lg:px-4 ">
+              <div className="h-60 overflow-y-auto ... lg:py-1 lg:px-4 max-w-xs ">
                 {players?.map((p) => (
                       
-                  <div className="max-w-sm rounded shadow-lg ">
+                  <div className="max-w-xs rounded shadow-lg ">
                     <div className="py-4 ">
-                      <div className="font-bold text-xs mb-2 ">{p}</div>
+                      <div className="font-bold text-xs mb-2 truncate ... indent-2 ">{p}</div>
                     </div>
-                    <div className="px-6 pt-4 pb-2">
+                    <div className="pt-4 pb-2">
                       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#value</span>
                     </div>
                   </div>
 
                 ))}
               </div>
-
             </div>
+
             
             {/* duplicar a div de cima quando estiver estilizada */}
 
