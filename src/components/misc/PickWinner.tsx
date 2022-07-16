d80f773e0ba751962cfd460a87f4d1c950fa310d
Matheus Martini <matheus.martini@rede.ulbra.br>
2022-07-16 14:43:06 -0300

    reload page

--- src/components/misc/PickWinner.tsx
+++ src/components/misc/PickWinner.tsx
@@ -5,13 +5,16 @@ const PickWinner = ({contracts}) => {
   const web3 = useWeb3React();
   const contract = useLottery(contracts);
 
+  const pick = async () => {
+    await contract.methods.pickWinner().send({
+      from: web3.account,
+    });
+    window.location.reload()
+}
+
   return !contract ? null : (
     <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-    onClick={() => {
-        contract.methods.pickWinner().send({
-          from: web3.account,
-        });
-      }}
+    onClick={pick}
     >
       Pick Winner
     </button>
